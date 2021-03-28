90e6c43e7af1e05f132cf04c4961554c1a71eb30
Hoang Viet <51878941+TheMarvelousWhale@users.noreply.github.com>
2021-03-28 09:19:52 +0800

    NAN

--- L6/untitled0.py
+++ L6/untitled0.py
@@ -1,43 +1,12 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Mar 18 11:32:41 2021
+Created on Fri Mar 26 16:03:36 2021
 
 @author: hoang
 """
 
-charset = "abcdefghijklmnopqrstuvwxyz"
-
-def insert(input_str,output):
-    for c in charset:
-        for insertion_index in range(len(input_str)+1):
-            inserted = input_str[:insertion_index] + c + input_str[insertion_index:]
-            print(f"{inserted} - {output}")
-            if inserted == output:
-                return True
-    return False
-
-def delete(input,output):
-    for deletion_index in range(len(input)):
-        deleted = input[:deletion_index] +input[deletion_index+1:]
-        if deleted == output:
-            return True
-    return False
-
-def replace(input,output):
-    for c in charset:
-        for replace_index in range(len(output)):
-            replaced = input[:]
-            replaced[replace_index] = c
-            if replaced == output:
-                return True
-    return False
-
-def main(input_str,output_str):
-    if insert(input_str,output_str) == True or delete(input_str,output_str) == True or replace(input_str,output_str) == True:
-        return True
-    else:
-        return False
-
-if __name__ == "__main__":
-    #main()
-    pass
+d = int("0x44",16)
+a = int("0x24",16)
+c = int("0x84",16)
+s = int("0x21",16)
+print(chr((d^a)-s+c))
